221e72368f9f3025525d192232115f166275dcb3
onestrangetrumpet <tgirl88888@gmail.com>
2026-01-28 13:48:14 -0500

    01/28/2026
    Fixed settings page

--- Ultra Ball/Assets/Scripts/PlayerPreferences.cs
+++ Ultra Ball/Assets/Scripts/PlayerPreferences.cs
@@ -5,15 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class PlayerPreferences : MonoBehaviour
 {    
-    private bool isSceneLoaded = false;
+    public string Settings;
     void Start()
-    {   
-        isSceneLoaded = true;
-        SceneManager.LoadScene("Settings");
+    {
+        StartCoroutine(LoadAndUnloadRoutine(Settings));
+    }
 
-        if (isSceneLoaded)
+    IEnumerator LoadAndUnloadRoutine(string Settings)
+    {
+        // --- 1. Load the scene additively ---
+        // Use LoadSceneMode.Additive to keep the current scene loaded
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(Settings, LoadSceneMode.Additive);
+
+        // Wait until the loading operation is complete
+        while (!loadOperation.isDone)
         {
-            SceneManager.UnloadSceneAsync("Settings");
+            yield return null;
         }
+
+        Debug.Log(Settings + " loaded.");
+
+        // Optional: Perform actions with the loaded scene contents here (e.g., extract data)
+
+        // --- 2. Unload the scene ---
+        // Use UnloadSceneAsync to remove the scene
+        AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(Settings);
+
+        // Wait until the unloading operation is complete
+        while (!unloadOperation.isDone)
+        {
+            yield return null;
+        }
+
+        Debug.Log(Settings + " unloaded.");
     }
 }
